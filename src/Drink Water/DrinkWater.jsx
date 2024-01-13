d207d40ae9783f060813be375674a3329cd6a928
Mr-Jitesh9170 <mr.jiteshpandey9170@gmail.com>
2024-01-14 03:42:19 +0530

    "final commit"

--- src/Drink Water/DrinkWater.jsx
+++ src/Drink Water/DrinkWater.jsx
@@ -1,33 +1,34 @@
 import { useState } from "react"
 import "./DrinkWater.scss"
 export const DrinkWater = () => {
-  const [click, setClick] = useState(0)
+  const [click, setClick] = useState("")
   return (
     <div className="drink-water-container">
+      <h2 className="limit">Drink Water!</h2>
+      <p className="goal">Goal: 2 Liters</p>
       <div className="drink-water-top">
-        <div className="limit">Drink Water!</div>
-        <div className="goal">Goal: 2 Liters</div>
-        <div className="water-glass">
-          <div className="water-line" style={{ bottom: `calc(12.5% * ${click})` }}>
-            <div>2L</div>
-            <span>Remained</span>
-          </div>
+        <div className="water-empty" style={{ height: `calc(100% - 12.5% * ${click})` }}>
+          <span className="liter">{2 - 0.25 * click}L</span>
+          <span className="remained">remained</span>
         </div>
-      </div>
-      <div className="drink-water-bottom">
-        <p>Select how many glasses of water that you have drank</p>
-        <div className="selected-water-glass">
+        <div className="water-filled" style={{ height: `calc(12.5% * ${click})` }}>
           {
-            selectedWater.map((_, i) => {
-              return <span className="small-glasses" onClick={() => {
-                setClick(i + 1)
-              }}>{_}</span>
-            })
+            click == 0 ? <span className="percentage"></span> : <span className="percentage">{12.5 * click}%</span>
           }
         </div>
       </div>
-    </div>
+      <p>select how much bottol you have drunk water</p>
+      <div className="drink-water-bottom">
+        {
+          selectedWater.map((_, i) => {
+            return <span className="small-bottel" style={click > i ? { backgroundColor: "#ffffffb7", color: "#144fc6" } : { background: "steetblue", color: "#fff" }} onClick={() => {
+              setClick(i + 1)
+            }}>{_}</span>
+          })
+        }
+      </div>
+    </div >
   )
 }
-
+//5.5125
 const selectedWater = ["250 ml", "250 ml", "250 ml", "250 ml", "250 ml", "250 ml", "250 ml", "250 ml",]
